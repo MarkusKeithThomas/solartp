31f9dabc9e9bc44a7517f8a933c1cc0b4b7d5d98
Chau_Hoai_Thuong <hthuongtn2905@gmail.com>
2025-03-05 20:22:15 +0700

    Thêm lại backend

---
+++ ecommerce-java/src/main/java/com/cybersoft/ecommerce/entity/UserEntity.java
@@ -0,0 +1,56 @@
+package com.cybersoft.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity(name = "users")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "oauth_id")
+    private String oauthId;
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private RoleEntity role;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public RoleEntity getRole() {
+        return role;
+    }
+
+    public void setRole(RoleEntity role) {
+        this.role = role;
+    }
+}
