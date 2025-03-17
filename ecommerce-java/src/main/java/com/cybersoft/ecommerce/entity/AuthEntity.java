2b160a67c0e654a5e535391aea338930afab720c
Chau_Hoai_Thuong <hthuongtn2905@gmail.com>
2025-03-17 10:12:42 +0700

    Delete ecommerce-java directory

--- ecommerce-java/src/main/java/com/cybersoft/ecommerce/entity/AuthEntity.java
+++
@@ -1,4 +0,0 @@
-package com.cybersoft.ecommerce.entity;
-
-public class AuthEntity {
-}
--- ecommerce-java/src/main/java/com/cybersoft/ecommerce/repository/AuthRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cybersoft.ecommerce.repository;
-
-import com.cybersoft.ecommerce.entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AuthRepository extends JpaRepository<UserEntity, Integer> {
-}
