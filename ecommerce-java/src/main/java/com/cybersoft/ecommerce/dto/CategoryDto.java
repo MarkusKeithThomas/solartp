58648a6b15507a392f6af1354cd02bc10b5858f3
Chau_Hoai_Thuong <hthuongtn2905@gmail.com>
2025-03-17 10:12:42 +0700

    Delete ecommerce-java directory

--- ecommerce-java/src/main/java/com/cybersoft/ecommerce/dto/CategoryDto.java
+++
@@ -1,9 +0,0 @@
-package com.cybersoft.ecommerce.dto;
-
-import lombok.Data;
-
-@Data
-public class CategoryDto {
-    private int categoryId;
-    private String name;
-}
--- ecommerce-java/src/main/java/com/cybersoft/ecommerce/request/ImageRequest.java
+++
@@ -1,9 +0,0 @@
-package com.cybersoft.ecommerce.request;
-
-import lombok.Data;
-
-@Data
-public class ImageRequest {
-    private String url;
-    private String allText;
-}
