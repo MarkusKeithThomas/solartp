fda94030485828b732225bcf713c0b8d01d513a8
Chau_Hoai_Thuong <hthuongtn2905@gmail.com>
2025-03-21 17:24:53 +0700

    update new fix

--- frontend-react/vite.config.ts
+++ frontend-react/vite.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: './',  // Giúp định tuyến đúng khi deploy lên Vercel
   plugins: [react()],
   server: {
+    port: 5173,
     proxy: {
       '/api': {
         target: 'http://localhost:8080', // Backend API
@@ -11,5 +13,5 @@ export default defineConfig({
         secure: false,
       }
     }
-  }
+  },
 });
