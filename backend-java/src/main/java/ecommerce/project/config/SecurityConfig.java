9cf43c4dcf52ae34996d0bb12a31b4e2f04724d0
Chau_Hoai_Thuong <hthuongtn2905@gmail.com>
2025-03-26 21:44:02 +0700

    fix robots

--- backend-java/src/main/java/ecommerce/project/config/SecurityConfig.java
+++ backend-java/src/main/java/ecommerce/project/config/SecurityConfig.java
@@ -60,7 +60,8 @@ public class SecurityConfig {
                             "/tai-khoan/google",
                             "/check-env",
                             "/tai-khoan/**",
-                            "/bai-viet/list/**"
+                            "/bai-viet/list/**",
+                            "/robots.txt"
                     ).permitAll(); // ✅ Cho phép OAuth2
                     auth.requestMatchers("/bai-viet/upload-excel").hasAuthority("ROLE_ADMIN");
                     auth.requestMatchers("/file/**").hasAuthority("ROLE_ADMIN");
