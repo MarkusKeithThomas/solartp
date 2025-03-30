6ee3bda62524689c1c8d16b850e1c480808f741d
Chau_Hoai_Thuong <hthuongtn2905@gmail.com>
2025-03-30 15:49:17 +0700

    fix login

--- frontend-react/src/pages/tai-khoan/LoginSuccess.tsx
+++ frontend-react/src/pages/tai-khoan/LoginSuccess.tsx
@@ -1,8 +1,11 @@
+import axios from "axios";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const LoginSuccess = () => {
     const navigate = useNavigate();
+    const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 
     useEffect(() => {
         const urlParams = new URLSearchParams(window.location.search);
@@ -10,9 +13,19 @@ const LoginSuccess = () => {
 
         if (accessToken) {
             localStorage.setItem("accessToken", accessToken);
-            console.log("Đăng nhập thành công! "+accessToken);
-            navigate("/"); // ✅ Chuyển đến trang chu
-        }
+            axios
+              .get(`${API_BASE_URL}/tai-khoan/user-info`, {
+                headers: { Authorization: `Bearer ${accessToken}` },
+              })
+              .then((response) => {
+                localStorage.setItem("user-info", JSON.stringify(response.data.data));
+                navigate("/");
+              })
+              .catch((err) => {
+                console.error("Lỗi khi lấy thông tin user sau login:", err);
+                navigate("/login"); // fallback nếu lỗi
+              });
+          }
     }, [navigate]);
 
     return <h2>Đang đăng nhập...</h2>;
